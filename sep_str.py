3537bfa86a8759a29aa9f031ca70600f7af9173e
Alex Van <alexhtwen@gmail.com>
2023-02-22 08:35:26 +0800

    more practices

--- sep_str.py
+++ sep_str.py
@@ -1,7 +1,6 @@
 import random as r
 
 def insert_sep(raw: str = '', sep: tuple | list = ()) -> str:
-    # print('/' + raw + '/')
     return '' if not sep else r.choice(sep).join(raw).strip()
 
 print()
@@ -9,11 +8,12 @@ print(f"{insert_sep() = }")
 print(f"{insert_sep('Python') = }")
 print(f"{insert_sep(sep=()) = }")
 print(f"{insert_sep('abcdefg', ('.', ',')) = }")
-print(f"{insert_sep('棄我去者昨日之日不可留', ('*', '_', '^')) = }")
-print(f"{insert_sep('亂我心者今日之日多煩憂', (' ',)) = }")
+print(f"{insert_sep('人有悲歡離合', ('*', '_', '^')) = }")
+print(f"{insert_sep('月有陰晴圓缺', (' ',)) = }")
 print(f"{insert_sep('123456', ('+', '-', '*', '/')) = }")
-print(f"{insert_sep('心肝脾肺腎 ', ('臟',)) = }")
-sep = (' 左中', ' 左食', ' 右中', ' 右食', ' 拇指')
+print(f"{insert_sep('心肝脾肺腎 ', ('臟 ',)) = }")
+sep = (' 肩胛', ' 下頜', ' 肋', ' 肱', ' 中節趾')
 print(f'{sep = }')
-print(f"{insert_sep(' 指指指指指指指', sep) = }")
+print(f"{insert_sep(' 骨骨骨骨', sep) = }")
+print()
 print()
