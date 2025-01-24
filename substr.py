881c884b39bff4499c979796780d7486955cab07
alex <alexhtwen@gmail.com>
2025-01-24 19:54:21 +0800

    minors

---
+++ substr.py
@@ -0,0 +1,11 @@
+str1 = 'Alex is a Pythonista.'
+
+
+substr = str1[0:]
+print(substr, substr is 'Alex is a Pythonista.')
+
+substr = str1[2:3]
+print(substr, substr is 'e')
+
+substr = str1[1:3]
+print(substr, substr is 'le')
